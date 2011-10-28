098ccdcaeac81ca6252326ee8a5f13845f12ccca
MWeigert <MWeigert@.eu.Micros.int>
2011-10-28 17:24:37 +0200

    first steps

---
+++ memory.java
@@ -0,0 +1,14 @@
+/**
+ * 		Klasse welche den Speicher des Mini-Power-PC Projekts simuliert.
+ * 		Es wird eine HashMap verwendet welche die Speicheradressen 400
+ * 		bis 600 simuliert.
+ */
+package info.mini.power.pc;
+
+/**
+ * @author Mathias Weigert & Miro Ljubicic
+ *
+ */
+public class memory {
+
+}
